75c491ed5aacd477c152f01f0257ef1f9905b411
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-02-18 23:15:58 +0100

    WebHook::resolves returns hydrated webhook

--- src/OpenAPI/WebHookInterface.php
+++ src/OpenAPI/WebHookInterface.php
@@ -7,7 +7,6 @@ interface WebHookInterface
     /**
      * @param array<string, string> $headers
      * @param array<mixed> $data
-     * @return string
      */
-    public function resolve(array $headers, array $data) : string;
+    public function resolve(array $headers, array $data) : object;
 }
