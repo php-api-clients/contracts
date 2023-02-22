9490b16f89d2b7431d9fea8fe564abb1b6fffe07
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-02-22 22:36:07 +0100

    Add WebHook hydrate/serialize methods

--- src/OpenAPI/WebHookInterface.php
+++ src/OpenAPI/WebHookInterface.php
@@ -5,6 +5,18 @@ namespace ApiClients\Contracts\OpenAPI;
 interface WebHookInterface
 {
     /**
+     * @template H
+     * @param class-string<H> $className
+     * @return H
+     */
+    public function hydrateWebHook(string $className, array $data) : object;
+
+    /**
+     * @return array{className: class-string, data: mixed}
+     */
+    public function serializeWebHook(object $object) : array;
+
+    /**
      * @param array<string, string> $headers
      * @param array<mixed> $data
      */
